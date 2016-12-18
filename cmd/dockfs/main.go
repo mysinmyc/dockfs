2a769cb4d4118ea4dd692cc9c84cadfcf0f190a6
mysinmyc <mysinmyc@users.noreply.github.com>
2016-12-18 21:09:27 +0100

    added -action umount

--- cmd/dockfs/main.go
+++ cmd/dockfs/main.go
@@ -9,42 +9,66 @@ import(
 	"github.com/mysinmyc/dockfs"
 )
 
+
+func mount(pMountPoint string) {
+
+	diagnostic.LogInfo("mount", "Creating docker client...")
+	vClient, vClientError := client.NewEnvClient() 
+	diagnostic.LogFatalIfError(vClientError,"mount","Failed to create client")
+	 
+	diagnostic.LogInfo("mount", "Creating filesystem...")
+	vFs,vFsError:= dockfs.NewDockFs(vClient)	
+	diagnostic.LogFatalIfError(vFsError,"mount","Failed to create filesystem")
+		
+	diagnostic.LogInfo("mount", "Mounting filesystem on %s...", pMountPoint)
+        vFileSystemConnection, vMountError := fuse.Mount(
+                pMountPoint,
+                fuse.FSName("dockfs"),
+                fuse.ReadOnly(),
+                fuse.LocalVolume())
+	diagnostic.LogFatalIfError(vMountError,"mount","An error occurred while mounting dockfs filesystem")
+       
+        defer vFileSystemConnection.Close()
+
+	diagnostic.LogInfo("mount", "Filesystem mounted")
+        vServeError := fs.Serve(vFileSystemConnection, vFs )
+	diagnostic.LogFatalIfError(vServeError,"mount", "An error occurred while serving filesystem")
+
+        <-vFileSystemConnection.Ready
+        if vFileSystemConnection.MountError != nil {
+                diagnostic.LogFatal("mount", "Mount failed", vFileSystemConnection.MountError)
+
+        }
+
+}
+
+func umount(pMountPoint string) {
+	diagnostic.LogInfo("umount", "Unmounting %s",pMountPoint)
+	vUmountError := fuse.Unmount(pMountPoint)
+	diagnostic.LogFatalIfError(vUmountError,"umount","An error occurred while umounting dockfs filesystem at %s",pMountPoint)
+}
+
 func main() {
 
 	vMountPointParameter := flag.String("mountPoint", "", "Target mountpoint")
+	vActionParameter := flag.String("action", "mount", "action [mount|umount]")
 	flag.Parse()
 
 	if *vMountPointParameter == "" {
 		diagnostic.LogFatal("main","missing mountPoint parameter",nil)
 	}
 
-	diagnostic.LogInfo("main", "Creating docker client...")
-	vClient, vClientError := client.NewEnvClient() 
-	diagnostic.LogFatalIfError(vClientError,"main","Failed to create client")
-	 
-	diagnostic.LogInfo("main", "Creating filesystem...")
-	vFs,vFsError:= dockfs.NewDockFs(vClient)	
-	diagnostic.LogFatalIfError(vFsError,"main","Failed to create filesystem")
-		
-	diagnostic.LogInfo("main", "Mounting filesystem on %s...", *vMountPointParameter)
-        vFileSystemConnection, vMountError := fuse.Mount(
-                *vMountPointParameter,
-                fuse.FSName("dockfs"),
-                fuse.ReadOnly(),
-                fuse.LocalVolume())
-	diagnostic.LogFatalIfError(vMountError,"main","An error occurred while mounting dockfs filesystem")
-       
-        defer vFileSystemConnection.Close()
 
-	diagnostic.LogInfo("main", "Filesystem mounted")
-        vServeError := fs.Serve(vFileSystemConnection, vFs )
-	diagnostic.LogFatalIfError(vServeError,"main", "An error occurred while serving filesystem")
-
-        <-vFileSystemConnection.Ready
-        if vFileSystemConnection.MountError != nil {
-                diagnostic.LogFatal("main", "Mount failed", vFileSystemConnection.MountError)
-
-        }
+	switch *vActionParameter {
+		case "mount":
+			mount(*vMountPointParameter)
+			break
+		case "umount":
+			umount(*vMountPointParameter)
+			break
+		default:
+			diagnostic.LogFatal("main", "invalid action %s",nil, *vActionParameter)
+	}
 
 	
 }
